d1c73ed3a716a976fa0df31fbaa2382a8db1dd75
olibeprado <olibeprado@gmail.com>
2026-03-08 20:36:43 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="Sistema Singularidade Olivan", layout="wide")
 st.title("🚀 Sistema Singularidade Olivan")
 st.subheader("Scanner de Tendência")
 
-moedas = ["bitcoin","ethereum","solana","ripple","cardano","MNT","FET","BNB","RAY","TAI","KSM","RENDER"]  
+moedas = ["bitcoin","ethereum","solana","ripple","cardano","MNTUSDT","FET","BNB","RAY","TAI","KSM","RENDER"]  
 
 def pegar_dados(moeda):
 
