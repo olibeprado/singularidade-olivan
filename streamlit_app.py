28cec5d5cc637839f77889d32c91b8997d438314
olibeprado <olibeprado@gmail.com>
2026-03-08 19:40:48 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ def pegar_dados():
     parametros = {
         "vs_currency": "usd",
         "order": "market_cap_desc",
-        "per_page": 200,
+        "per_page": 190,
         "page": 1
     }
 
