564d726d461d0d1da0a4a5a5044da142f3dde576
olibeprado <olibeprado@gmail.com>
2026-03-14 23:03:36 -0300

    Update page.tsx

--- atlas-web/app/page.tsx
+++ atlas-web/app/page.tsx
@@ -1,5 +1,9 @@
 import AtlasChartPro2 from "../components/AtlasChartPro2";
 
 export default function Page() {
-  return <AtlasChartPro2 />;
+  return (
+    <main style={{ width: "100%", height: "100vh" }}>
+      <AtlasChartPro2 />
+    </main>
+  );
 }
