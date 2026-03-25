fbb24b6e72e4a1923f019a4142dae0a761ba1fa0
olibeprado <olibeprado@gmail.com>
2026-03-25 06:48:55 -0300

    Update AtlasChartPro2.tsx

--- atlas-web/components/AtlasChartPro2.tsx
+++ atlas-web/components/AtlasChartPro2.tsx
@@ -5161,8 +5161,10 @@ export default function AtlasChartPro2() {
       <ModuleStrip activeModule={activeModule} onChange={setActiveModule} />
 
       <div style={{ display: "flex", minHeight: 0, flex: 1 }}>
-        <DrawingToolbar activeTool={drawingState.activeTool} onChangeTool={drawingState.setActiveTool} />
-
+       <ToolsSidebar
+        activeTool={drawingState.activeTool}
+        onChangeTool={drawingState.setActiveTool}
+     />
         <div style={{ flex: 1, minWidth: 0, minHeight: 0 }}>
           <div
             style={{
