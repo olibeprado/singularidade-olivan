d2a169963e57e645acdf987c60b71bbb18ae64be
olibeprado <olibeprado@gmail.com>
2026-03-25 06:24:27 -0300

    Update AtlasChartPro2.tsx

--- atlas-web/components/AtlasChartPro2.tsx
+++ atlas-web/components/AtlasChartPro2.tsx
@@ -1,13 +1,7 @@
 "use client";
 
 import React, { useEffect, useMemo, useRef, useState, useCallback } from "react";
-import {
-  createChart,
-  ColorType,
-  CrosshairMode,
-  IChartApi,
-  Time,
-} from "lightweight-charts";
+import { createChart, ColorType, CrosshairMode, IChartApi, Time } from "lightweight-charts";
 import {
   Activity,
   BarChart2,
